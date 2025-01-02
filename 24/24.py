c0a4154f9fc4eff3ebcbe87e6238259c8fa479bb
Tymoteusz <tymoteusz.ciesielski@ing.com>
2025-01-02 20:26:04 +0100

    24 2nd part updated but still WIP

--- 24/24.py
+++ 24/24.py
@@ -43,24 +43,51 @@ def solve_first(lines):
         if i[0] == 'z':
             outputs.append([i, j])
     outputs.sort(reverse=True)
-    acc = int("".join(str(output[1]) for output in outputs), 2) #We start appending with the most significant bit
+    acc = "".join(str(output[1]) for output in outputs) #We start appending with the most significant bit
+    print(len(acc))
     return acc
 
 
 def solve_second(lines):
-    acc = ""
+    # How to sum 2 binary number?
+    # We need to perform 2 operations: 1st summing the numbers - for that we use XOR. This is the "sum" result
+    # 2nd we compute the "carry" (przeniesienie) with the and gate
+    # At the end, we sum the next more significant bits + the carry
     inputs = dict()
     outputs = []
     gates = []
+    acc = 0
     for i in range(len(lines)):
         if lines[i] == "":
             break
         else:
             x, y = lines[i].split(":")
             inputs[x] = int(y.strip())
-    for i in reversed(range(1, int(len(inputs)/2)),):
+    # How to find out if a wire is switched?
+    # If the final output for a given z-bit is incorrect (downstream wires must have been switched)
+    # If the final output bit is correct 0 downstream might have been switched (or not!)
+    x, y = "", ""
+    for i in reversed(range(int(len(inputs)/2))):
         s = f"0{i}" if i <10 else str(i)
-        acc+=("1" if (inputs[f"x{s}"] == 1 and inputs[f"y{s}"]==1) else "0")
+        # acc+=("1" if (inputs[f"x{s}"] != inputs[f"y{s}"]==1) else "0")
+        x+= str(inputs[f"x{s}"])
+        y+= str(inputs[f"y{s}"])
+    print(f"X: {int(x, 2)}")
+    print(f"Y: {int(y, 2)}")
+    #print("Z1: 1110101110010100010001001110110001010001110000") #Original output
+    print("Z1: 1110101110010100010001010000110001010001110000")
+    print(f"Z2: {'{0:b}'.format(int(x, 2)+int(y, 2))}")
+    z1 = '1110101110010100010001001110110001010001110000'
+    z2 = '{0:b}'.format(int(x, 2)+int(y, 2))
+    for i in range(len(z1)):
+        if z1[i] != z2[i]:
+            print(abs(i-45))
+            # After printing, turns out that at least 11 outputs are incorrect (going downstream)
+            # X19 y19 is for sure incorrect. z19 should take into account third input (carry) and instead it is calculated using x19 and y19. (I think) this result should be instead upstream for z20 (carry)
+            # ckt XOR jwg (ckt is xor for x20 y20)
+            # jwg should be output of x19 AND y19
+            # bnh OR sbg -> jwg ()
+            # z-value should never be output of AND operation, always XOR of a sum and carry (EDIT: but there can be some extra operations before...)
     return acc
 
 
