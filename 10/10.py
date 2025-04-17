75e72cbed668ba78ea299395b5439d825c7f7865
Tymoteusz <tymoteusz.ciesielski@ing.com>
2025-04-17 16:57:59 +0200

    Solved 1 and 2

--- 10/10.py
+++ 10/10.py
@@ -1,47 +1,61 @@
-
 def parse():
-    return open("input_small.txt").read().splitlines()
+    return open("input.txt").read().splitlines()
+
 
 def is_in_bound(cord_x, cord_y, board):
-    if cord_x>=len(board) or cord_x <0 or cord_y >=len(board[0]) or cord_y < 0:
+    if cord_x >= len(board) or cord_x < 0 or cord_y >= len(board[0]) or cord_y < 0:
         return False
     else:
         return True
 
-def follow_trail(cord_x, cord_y, board, previous_height, acc):
+
+def follow_trail(cord_x, cord_y, board, previous_height, trs, p_1):
     current_height = int(board[cord_x][cord_y])
-    if current_height != previous_height + 1: # Not a part of a good trail
-        return acc
-    elif board[cord_x][cord_y] == '9':
-        print(f"Found the end of a trail! x {cord_x} y {cord_y}")
-        return acc+1
+    if current_height != previous_height + 1:  # Not a part of a good trail
+        return trs
+    elif board[cord_x][cord_y] == "9":
+        if (cord_x, cord_y) in trs and p_1:
+            return trs
+            # print(trs)
+            # print(f"Found the end of a trail! x {cord_x} y {cord_y}")
+        trs.append((cord_x, cord_y))
+        return trs
     for i in (-1, 1):
         if is_in_bound(cord_x + i, cord_y, board):  # going upwards or downwards
-            acc = follow_trail(cord_x + i, cord_y, board, current_height, acc)
+            trs = follow_trail(cord_x + i, cord_y, board, current_height, trs, p_1)
         if is_in_bound(cord_x, cord_y + i, board):  # going left or right
-            acc = follow_trail(cord_x, cord_y + i, board, current_height, acc)
-    return acc
+            trs = follow_trail(cord_x, cord_y + i, board, current_height, trs, p_1)
+    return trs
 
-def count_trails_for_trailhead(cord_x, cord_y, board, acc):
-    if board[cord_x][cord_y] != '0':
+
+def count_trails_for_trailhead(cord_x, cord_y, board, p_1):
+    if board[cord_x][cord_y] != "0":
         raise Exception("Incorrect trailhead")
-    for i in (-1,1):
-        if is_in_bound(cord_x+i,cord_y, board): #going upwards or downwards
-            acc = follow_trail(cord_x+i, cord_y, board, 0, acc)
-        if is_in_bound(cord_x, cord_y+i, board): #going left or right
-            acc = follow_trail(cord_x, cord_y+i, board, 0, acc)
-    return acc
+    trs = []
+    for i in (-1, 1):
+        if is_in_bound(cord_x + i, cord_y, board):  # going upwards or downwards
+            trs = follow_trail(cord_x + i, cord_y, board, 0, trs, p_1)
+        if is_in_bound(cord_x, cord_y + i, board):  # going left or right
+            trs = follow_trail(cord_x, cord_y + i, board, 0, trs, p_1)
+    return len(trs)
+
 
 def solve_first(lines):
     acc = 0
-    for x,l in enumerate(lines):
-        for y,c in enumerate(l):
-            if c == '0':
-                acc = count_trails_for_trailhead(x,y,lines,acc)
+    for x, l in enumerate(lines):
+        for y, c in enumerate(l):
+            if c == "0":
+                acc += count_trails_for_trailhead(x, y, lines, p_1=True)
     return acc
 
+
 def solve_second(lines):
-    pass
+    acc = 0
+    for x, l in enumerate(lines):
+        for y, c in enumerate(l):
+            if c == "0":
+                acc += count_trails_for_trailhead(x, y, lines, p_1=False)
+    return acc
 
 
 print(solve_first(parse()))
